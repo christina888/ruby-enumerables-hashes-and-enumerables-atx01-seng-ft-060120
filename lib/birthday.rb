75670652f6f17551e4607e84a1d878d482a34e81
Christina Chianelli <cechianelli@gmail.com>
2020-05-26 20:58:59 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,7 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |name, age|
-  message = 
+  message = "Happy Birthday #{name}! You are now #{age} years old"
 end
   puts message
 end
